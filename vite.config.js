3921c78fe67046a6a0f3b737a1014e0a2df4e29c
03-renate <renate.pedersen03@gmail.com>
2025-04-28 14:30:28 +0200

    chore: add project structure to repo

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+//This code comes from the js-project, created by/taken from Monde Sineke
+
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      },
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+});
